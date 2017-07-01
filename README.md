34d4ac8b631034df50bf0cdc98ed373eefd00f08
Sylvain Metayer <sylvain_metayer@outlook.fr>
2017-07-01 14:57:36 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # semaine-innovante-dawin
 Dépôt pour la semaine innovante de la LP DAWIN
+
+# Requirements
+
+- Nodejs / npm
+
+# Setup
+
+## Clone
+- `git clone https://github.com/sylvainmetayer/semaine-innovante-dawin.git`
+
+OU
+
+- `git clone git@github.com:sylvainmetayer/semaine-innovante-dawin.git`
+
+## Install dependencies
+
+- `npm install`
+
+## Gulp tasks
+
+- `gulp deploy` : Will deploy to the github page repo
+
+- `gulp build` : Will build a production version, located in dist/ folder
+
+- `gulp serve:dist` : Will run a external production version accessible on port 9000
+
+- `gulp serve` : Run on port 9000 the app (dev mode). Auto reload on change.
+
+- `gulp clean` : Clean all temporary files.
---
+++ app/scripts/fitbit.js
@@ -0,0 +1,76 @@
+// If user hasn't authed with Fitbit, redirect to Fitbit OAuth Implicit Grant Flow
+var fitbitAccessToken;
+
+if (!window.location.hash) {
+    window.location.replace('https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=228FYH&redirect_uri=https%3A%2F%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight');
+} else {
+    var fragmentQueryParameters = {};
+    window.location.hash.slice(1).replace(
+        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+        function($0, $1, $2, $3) { fragmentQueryParameters[$1] = $3; }
+    );
+
+    fitbitAccessToken = fragmentQueryParameters.access_token;
+}
+
+// Make an API request and graph it
+var processResponse = function(res) {
+    if (!res.ok) {
+        throw new Error('Fitbit API request failed: ' + res);
+    }
+
+    var contentType = res.headers.get('content-type')
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+        return res.json();
+    } else {
+        throw new Error('JSON expected but received ' + contentType);
+    }
+}
+
+var processHeartRate = function(timeSeries) {
+    return timeSeries['activities-heart-intraday'].dataset.map(
+        function(measurement) {
+            return [
+                measurement.time.split(':').map(
+                    function(timeSegment) {
+                        return Number.parseInt(timeSegment);
+                    }
+                ),
+                measurement.value
+            ];
+        }
+    );
+}
+
+var graphHeartRate = function(timeSeries) {
+    console.log(timeSeries);
+    var data = new google.visualization.DataTable();
+    data.addColumn('timeofday', 'Time of Day');
+    data.addColumn('number', 'Heart Rate');
+
+    data.addRows(timeSeries);
+
+    var options = google.charts.Line.convertOptions({
+        height: 450
+    });
+
+    var chart = new google.charts.Line(document.getElementById('chart'));
+
+    chart.draw(data, options);
+}
+
+fetch(
+    'https://api.fitbit.com/1/user/-/activities/heart/date/2016-03-19/1d/1sec/time/21:00/23:00.json',
+    {
+        headers: new Headers({
+            'Authorization': 'Bearer ' + fitbitAccessToken
+        }),
+        mode: 'cors',
+        method: 'GET'
+    }
+).then(processResponse)
+    .then(processHeartRate)
+    .then(graphHeartRate)
+    .catch(function(error) {
+        console.log(error);
+    });
