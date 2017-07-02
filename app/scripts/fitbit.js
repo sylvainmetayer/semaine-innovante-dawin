e46321b5c2a343c562900b2e367e3ef3c82fa731
Sylvain METAYER <sylvain_metayer@outlook.fr>
2017-07-02 18:09:48 +0200

    Add dev mode from localhost

--- app/scripts/fitbit.js
+++ app/scripts/fitbit.js
@@ -1,64 +1,52 @@
 // If user hasn't authed with Fitbit, redirect to Fitbit OAuth Implicit Grant Flow
 var fitbitAccessToken;
 
-if (!window.location.hash) {
-    window.location.replace('https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=228FYH&redirect_uri=https%3A%2F%2Fsylvainmetayer.github.io%2Fsemaine-innovante-dawin%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight');
-} else {
-    var fragmentQueryParameters = {};
-    window.location.hash.slice(1).replace(
-        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-        function($0, $1, $2, $3) { fragmentQueryParameters[$1] = $3; }
-    );
+let app_client_id = "228FYH";
 
-    fitbitAccessToken = fragmentQueryParameters.access_token;
+let login_url = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=" + app_client_id + " &redirect_uri=https%3A%2F%2Fsylvainmetayer.github.io%2Fsemaine-innovante-dawin%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight";
+
+var dev_token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1VDc1UzgiLCJhdWQiOiIyMjhGWUgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTMwNTQ3NTgyLCJpYXQiOjE0OTkwMTE1ODJ9.mPjMJHRPcZFZBXb9kRRcZYqRR6jLf6uhIO18Rk_vUtE";
+
+var isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
+if (isLocal) {
+    fitbitAccessToken = dev_token;
+} else {
+    if (!window.location.hash) {
+        window.location.replace(login_url);
+    } else {
+        var fragmentQueryParameters = {};
+        window.location.hash.slice(1).replace(
+            new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+            function ($0, $1, $2, $3) {
+                fragmentQueryParameters[$1] = $3;
+            }
+        );
+
+        fitbitAccessToken = fragmentQueryParameters.access_token;
+        console.log(fitbitAccessToken);
+    }
 }
 
+
 // Make an API request and graph it
-var processResponse = function(res) {
+let processResponse = function (res) {
     if (!res.ok) {
         console.log(res);
         throw new Error('Fitbit API request failed: ' + res);
     }
 
-    var contentType = res.headers.get('content-type')
+    let contentType = res.headers.get('content-type');
     if (contentType && contentType.indexOf("application/json") !== -1) {
         return res.json();
     } else {
         throw new Error('JSON expected but received ' + contentType);
     }
-}
+};
 
-var processHeartRate = function(timeSeries) {
-    return timeSeries['activities-heart-intraday'].dataset.map(
-        function(measurement) {
-            return [
-                measurement.time.split(':').map(
-                    function(timeSegment) {
-                        return Number.parseInt(timeSegment);
-                    }
-                ),
-                measurement.value
-            ];
-        }
-    );
-}
-
-var graphHeartRate = function(timeSeries) {
-    console.log(timeSeries);
-    var data = new google.visualization.DataTable();
-    data.addColumn('timeofday', 'Time of Day');
-    data.addColumn('number', 'Heart Rate');
-
-    data.addRows(timeSeries);
-
-    var options = google.charts.Line.convertOptions({
-        height: 450
-    });
-
-    var chart = new google.charts.Line(document.getElementById('chart'));
-
-    chart.draw(data, options);
-}
+let processHeartRate = function (jsonData) {
+    console.log(jsonData);
+};
 
 fetch(
     'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json',
@@ -71,7 +59,6 @@ fetch(
     }
 ).then(processResponse)
     .then(processHeartRate)
-    .then(graphHeartRate)
-    .catch(function(error) {
+    .catch(function (error) {
         console.log(error);
     });
