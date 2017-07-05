db1200f010a2b3efe8771e697d1ce51f2f897f8a
Romain Durade <durade.romain@gmail.com>
2017-07-05 22:03:15 +0200

    Date validator à revoir

--- app/api/bin/form/InsertResultForm.php
+++ app/api/bin/form/InsertResultForm.php
@@ -2,14 +2,39 @@
 
 namespace form;
 
+use lib\Field;
+use validator\DateTimeValidator;
+use validator\PositiveIntegerValidator;
+use validator\NotNullValidator;
+
 /**
 * Formulaire
 */
-class InsertResultForm extends GetResultForm
+class InsertResultForm extends SelectResultForm
 {
 	public function __construct() {
 		parent::__construct();
 
 		return $this;
 	}
+
+    public function build() {
+	    parent::build();
+
+        $this->addField( new Field('thedate') )
+            ->addValidator(new NotNullValidator("Date not null"))
+            ->addValidator(new DateTimeValidator("Format de la date invalide", "d-m-Y H:i:s"));
+
+        $this->addField( new Field('first_hr') )
+            ->addValidator(new NotNullValidator("Id fit not null"))
+            ->addValidator(new PositiveIntegerValidator("Invalid integer"));
+
+        $this->addField( new Field('second_hr') )
+            ->addValidator(new NotNullValidator("Id fit not null"))
+            ->addValidator(new PositiveIntegerValidator("Invalid integer"));
+
+        $this->addField( new Field('third_hr') )
+            ->addValidator(new NotNullValidator("Id fit not null"))
+            ->addValidator(new PositiveIntegerValidator("Invalid integer"));
+    }
 }
