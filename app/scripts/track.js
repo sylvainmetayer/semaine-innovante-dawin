69bdee332e8feea232d17442b684ba305854b856
Sylvain METAYER <sylvain_metayer@outlook.fr>
2017-07-05 11:07:07 +0200

    Add statistics

---
+++ app/scripts/track.js
@@ -0,0 +1,17 @@
+var _paq = _paq || [];
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setDoNotTrack", true]);
+_paq.push(['enableLinkTracking']);
+_paq.push(['enableHeartBeatTimer', 30]);
+_paq.push(['trackPageView']);
+(function () {
+    var u = "//analytics.sylvainmetayer.fr/";
+    _paq.push(['setTrackerUrl', u + 'piwik.php']);
+    _paq.push(['setSiteId', '7']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript';
+    g.async = true;
+    g.defer = true;
+    g.src = u + 'piwik.js';
+    s.parentNode.insertBefore(g, s);
+})();
